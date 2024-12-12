fd33864fa67792f0cfe2396b74037463c807f379
Demasse <demassedimas2006@gmail.com>
2024-12-12 10:39:29 +0100

    second commit

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -74,7 +74,7 @@ class UserResource extends Resource
             ])
             ->actions([
                 Tables\Actions\ActionGroup::make([
-                    
+
                 Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
                 Tables\Actions\DeleteAction::make(),
